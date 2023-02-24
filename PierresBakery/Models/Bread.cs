5d8a57052c40299947b4bf8b9b3f881a32bc58a0
Molly Donegan <mollyrdonegan@gmail.com>
2023-02-24 09:42:58 -0800

    pass first TotalCost test

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -2,11 +2,14 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    // public static int Price { get; get; } = 5;
+    public static int Price { get; set; } = 5;
 
     public static int TotalCost(int quantity)
     {
       int finalCost = 0;
+      if (quantity == 1){
+        finalCost += Price;
+      }
       return finalCost;
     }
   }
